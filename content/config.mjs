52d4480dba8a9147e33c06f34fe658952681c652
DarrenZal <zaldarren@gmail.com>
2024-10-16 00:25:54 -0700

    initial commit

--- content/config.mjs
+++ content/config.mjs
@@ -1,5 +1,17 @@
-
-/**
- * @type {import("../config/siteConfig").UserConfig}
- */
-export default {};
+const config = {
+    // title will be displayed on the top of your site
+    title: "My Awesome Blog",
+    // adding a description helps with SEO
+    description: "This is my awesome blog built with Flowershow",
+    // author of site displayed on the bottom of your site
+    author: "Darren",
+    // logo image
+    authorLogo: "/images/logo.svg",
+    // url to author website
+    domain: "https://john.app/",
+    // links to the pages you want to link to in the navbar
+    navLinks: [{ href: "/about", name: "About" }],
+  };
+  
+  export default config;
+  
